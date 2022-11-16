96d746e1661f33105a2c90c349412956931759c7
Vineeth Balaji <Vineeth_Balaji@edwards.com>
2022-11-16 13:10:16 +0530

    version updated

--- src/main/java/com/naveen/cicd/GithubCicdActionsApplication.java
+++ src/main/java/com/naveen/cicd/GithubCicdActionsApplication.java
@@ -11,7 +11,7 @@ public class GithubCicdActionsApplication {
 
 	@GetMapping("/welcome")
 	public String welcome(){
-		return "Welcome to Sample springboot app for Signiance-v2 !";
+		return "Welcome to Sample springboot app for Signiance-v3 !";
 	}
 
 
